bce78421242f3a0abf588b0ed848d727e55b5827
akhalifa7 <57017131+akhalifa7@users.noreply.github.com>
2019-11-08 22:16:46 +0200

    Deleting Original GUI

--- DS Project/GUI/GUIDefs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-enum GUI_REGION {
-	ORD_REG,	//GUI Regions where waiting orders are drawn
-	COOK_REG,	//GUI Regions where waiting cooks are drawn
-	SRV_REG,	//GUI Regions where in-service orders are drawn
-	DONE_REG,	//GUI Regions where finished orders are drawn
-	REG_CNT		//Total number of regions
-};
-
