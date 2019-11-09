d5149e94bf409540cd83a61b0e16c44c5e8d81b8
TasneemOmara <tsneem.omara@gmail.com>
2019-11-09 02:03:25 +0200

    fixing an error in cancellation order execute

--- DS Project/Events/CancellationEvent.cpp
+++ DS Project/Events/CancellationEvent.cpp
@@ -9,6 +9,6 @@ CancellationEvent::CancellationEvent(int eTime, int oID):Event(eTime, oID)
 
 void CancellationEvent::Execute(Restaurant* pRest)
 {
-	bool found = pRest->get_Normal_orders_list().DeleteNode(ID); //errors may rise due to operator overloading
+	bool found = pRest->get_Normal_orders_list().DeleteNode(this->getOrderID()); //errors may rise due to operator overloading
 
 }
