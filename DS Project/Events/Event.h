e82829539059a420cd06353a42870227e4a2a6ff
akhalifa7 <57017131+akhalifa7@users.noreply.github.com>
2019-12-17 04:53:08 +0200

    cc

--- DS Project/Events/Event.h
+++ DS Project/Events/Event.h
@@ -25,5 +25,4 @@ public:
 	virtual void setDishes(int size) {};
 	virtual void setMoney(double money) {};
 	virtual void Execute(Restaurant* pRest)=0;	////a pointer to "Restaurant" and events need it to execute
-
 };
